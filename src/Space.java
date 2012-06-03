864e4d3e6feb5bcfffdb9809091641c59286c07c
eiji.ienaga <eiji.ienaga@gmail.com>
2012-06-03 14:43:07 +0900

    誕生ケースを実装

--- src/Space.java
+++ src/Space.java
@@ -1,36 +1,98 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Space {
-	private static int ALIVE = 1;
-	private static int DEAD = 0;
 	
 	private Cell[][] cells;
-	public class Cell {
-		public final int status;
-		private Cell(int status) {
-			this.status = status;
-		}
-	};
-	
-	public Space(Integer[][] cells) {
-		this.cells = new Space.Cell[cells.length][];
-		for (int row = 0; row < cells.length; row++) {
-			this.cells[row] = new Space.Cell[cells[row].length];
-			for (int colum = 0; colum < cells[row].length; colum++) {
-				this.cells[row][colum] = new Cell(cells[row][colum]);
+	public Space(Cell[][] cells) {
+		this.cells = cells;
+	}
+
+	public Space(Integer[][] nodeCells) {
+		this.cells = new Space.Cell[nodeCells.length][];
+		for (int row = 0; row < nodeCells.length; row++) {
+			this.cells[row] = new Space.Cell[nodeCells[row].length];
+			for (int column = 0; column < nodeCells[row].length; column++) {
+				this.cells[row][column] = new Cell(nodeCells[row][column], row, column);
 			}
 		}
 	}
 
-	public Space(Space.Cell[][] cells) {
-		this.cells = cells;
-	}
-
 	public Space nextTime() {
-		// TODO
-		return new Space(this.cells);
+		Cell[][] nextCells = new Cell[this.cells.length][];
+		for (int row = 0; row < this.cells.length; row++) {
+			nextCells[row] = new Cell[this.cells[row].length];
+			for (int column = 0; column < this.cells[row].length; column++) {
+				nextCells[row][column] = this.cells[row][column].nextTime();
+			}
+		}
+		return new Space(nextCells);
 	}
 
-	public Space.Cell cell(int row, int colum) {
-		return new Space.Cell(ALIVE);
+	public Space.Cell cell(int row, int column) {
+		return cells[row][column];
+	}
+	
+	public class Cell {
+		private static final int ALIVE = 1;
+		public final int status;
+		public final int row;
+		public final int column;
+		private Cell(int status, int row, int column) {
+			this.status = status;
+			this.row = row;
+			this.column = column;
+		}
+
+		public Boolean isAlive() {
+			return !isDead();
+		}
+
+		public Boolean isDead() {
+			return this.status == 0;
+		}
+		
+		private Cell nextTime() {
+			if (countAroundAlive() == 3) {
+				return new Cell(ALIVE, this.row, this.column);
+			}
+			// TODO;
+			return this;
+		}
+
+		public int countAroundAlive() {
+			int counter = 0;
+			for (Cell cell : aroundCells()) {
+				if (cell.isAlive()) {
+					counter ++;
+				}
+			}
+			return counter;
+		}
+
+		public List<Cell> aroundCells() {
+			List<Cell> results = new ArrayList<Cell>();
+			for (int rowIndex = row - 1; rowIndex <= row + 1; rowIndex++) {
+				for (int columnIndex = column - 1; columnIndex <= column + 1; columnIndex++) {
+					if(rowIndex == row && columnIndex == column) {
+						continue;
+					}
+					if (inSapce(rowIndex, columnIndex)) { 
+						results.add(cells[rowIndex][columnIndex]);
+					}
+				}
+			}
+			return results;
+		}
+		
+		private boolean inSapce(int row, int column) {
+			return 0 <= row && 0 <= column &&
+				  row <= cells.length - 1 && column <= cells[row].length - 1;
+		}
+
+		@Override
+		public String toString() {
+			return "[staus:" + status + ", row:" + row + ", column:" + column + "]";
+		}
 	}
 }
--- src/SpaceTest.java
+++ src/SpaceTest.java
@@ -1,5 +1,9 @@
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
+import static org.junit.matchers.JUnitMatchers.hasItems;
+
+import java.util.List;
 
 import org.junit.Test;
 
@@ -7,12 +11,89 @@ import org.junit.Test;
 public class SpaceTest {
 	public static int ALIVE = 1;
 
+	private Integer[][] sampleCalse() {
+		return aliveCase();
+	}
+	
+	private Integer[][] aliveCase() {
+		return new Integer[][] {{1,1,0},
+				                {1,0,0},
+								{0,0,0}};
+	}
+
 	@Test
-	public void test誕生_死んでいるセルに隣接する生きたセルがちょうど3つあれば次の世代が誕生する() {
-		Integer[][] cells = new Integer[][] {{1,1,0},
-				                             {1,0,0},
-											 {0,0,0}}; 
-		assertThat(new Space(cells).nextTime().cell(1,1).status, 
-				is(ALIVE));
+	public void 誕生_死んでいるセルに隣接する生きたセルがちょうど3つあれば次の世代が誕生する() {
+		assertThat(new Space(aliveCase()).nextTime().cell(1,1).isAlive(), 
+				is(true));
+	}
+	
+	// Cell#countAroundAlive()
+	@Test
+	public void 周りの生きているセルの数をカウントできること() throws Exception {
+		assertThat(new Space(aliveCase()).cell(1, 1).countAroundAlive(), is(3));
+	}
+	// Cell#aroundCells()
+	
+	@Test
+	public void 中央の場合の周りのセルが取得できること() throws Exception {
+		Space space = new Space(aliveCase());
+		List<Space.Cell> aroundCells = space.cell(1,1).aroundCells();
+		assertThat(aroundCells.size(), is(8));
+		assertThat(aroundCells, 
+				hasItems(space.cell(0, 0),
+						 space.cell(0, 1),
+						 space.cell(0, 2),
+						 space.cell(1, 0),
+						 space.cell(1, 2),
+						 space.cell(2, 0),
+						 space.cell(2, 1),
+						 space.cell(2, 2)));
+		assertThat(aroundCells, 
+				not(hasItems(
+						 space.cell(1, 1))));
+	}
+
+	@Test
+	public void 左上隅の場合の周りのセルが取得できること() throws Exception {
+		Space space = new Space(sampleCalse());
+		List<Space.Cell> aroundCells = space.cell(0, 0).aroundCells();
+		assertThat(aroundCells.size(), is(3));
+		assertThat(aroundCells, 
+				hasItems(space.cell(0, 1),
+						 space.cell(1, 0),
+						 space.cell(1, 1)));
+		assertThat(aroundCells, 
+				not(hasItems(
+						 space.cell(0, 0))));
+	}
+
+	@Test
+	public void 右下隅の場合の周りのセルが取得できること() throws Exception {
+		Space space = new Space(sampleCalse());
+		List<Space.Cell> aroundCells = space.cell(2, 2).aroundCells();
+		
+		assertThat(aroundCells.size(), is(3));
+		assertThat(aroundCells, 
+				hasItems(space.cell(1, 1),
+						 space.cell(1, 2),
+						 space.cell(2, 1)));
+		assertThat(aroundCells, 
+				not(hasItems(
+						 space.cell(2, 2))));
+	}
+
+	@Test
+	public void 右上隅の場合の周りのセルが取得できること() throws Exception {
+		Space space = new Space(sampleCalse());
+		List<Space.Cell> aroundCells = space.cell(0, 2).aroundCells();
+		
+		assertThat(aroundCells.size(), is(3));
+		assertThat(aroundCells, 
+				hasItems(space.cell(0, 1),
+						 space.cell(1, 1),
+						 space.cell(1, 2)));
+		assertThat(aroundCells, 
+				not(hasItems(
+						 space.cell(0, 2))));
 	}
 }
